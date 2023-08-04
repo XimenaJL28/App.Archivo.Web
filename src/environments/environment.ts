e1eda6e9cdd185e49ed0a4a153649cb98ac22d13
Mqe10 <mauricioquispeestrada@gmail.com>
2023-08-04 15:51:18 -0400

    Login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,126 +1,19 @@
 // export const environment = {
 //   production: false,
-//   endPoint: "https://localhost:7250/",
-//   urlLogin: 'https://portal.upds.edu.bo/web/SAADS/DEV/Login/SAADS-WEB?returnUrlLogin=http://localhost:4200/%23/login',
-//   urlAccess: 'https://portal.upds.edu.bo/api/dev/access/api/',
+//   endPoint: "https://api-saads-dev.upds.edu.bo/archivo/api/",
+//   urlLogin: 'https://login-dev.upds.edu.bo/SAADS-WEB?returnUrlLogin=http://localhost:4200/%23/login',
+//   urlAccess: 'https://api-saads-dev.upds.edu.bo/access/api/',
 //   urlFile: 'https://portal.upds.edu.bo/gapi.dev/upload/uploadasynccontainer/portal-notificacion',
-//   apikey: "5SJE8rmwaw4rKz",
-//   objModNotificaciones: {
-//     id: 34002,
-//     nombre: "Noticias",
-//     estado: true,
-//     interfaces: [
-//       {
-//         "id": 41004,
-//         "titulo": "Notificaciones",
-//         "descripcion": " ",
-//         "ubicacion": "notice",
-//         "enlaceTutorial": null,
-//         "tipo": 1,
-//         "idModulo": 34002,
-//         "icono": "pi-align-left",
-//         "estado": true,
-//         "tareas": [
-//           {
-//             "id": 40675,
-//             "nombre": "CREAR",
-//             "descripcion": "CREAR",
-//             "idInterface": 41004,
-//             "tipo": 1
-//           },
-//           {
-//             "id": 40676,
-//             "nombre": "EDITAR",
-//             "descripcion": "EDITAR",
-//             "idInterface": 41004,
-//             "tipo": 3
-//           },
-//           {
-//             "id": 40677,
-//             "nombre": "ELIMINAR",
-//             "descripcion": "ELIMINAR",
-//             "idInterface": 41004,
-//             "tipo": 4
-//           },
-//           {
-//             "id": 40678,
-//             "nombre": "LEER",
-//             "descripcion": "LEER",
-//             "idInterface": 41004,
-//             "tipo": 2
-//           },
-//           {
-//             "id": 40679,
-//             "nombre": "TODO",
-//             "descripcion": "LEER",
-//             "idInterface": 41004,
-//             "tipo": 5
-//           }
-//         ]
-//       }]
-//   }
+//   endPointAccess: "https://portal.upds.edu.bo/gapi.dev/request/service/?path=access/api/",
+//   endPointPortal: "http://172.16.248.33:8300/Portal/api/",
+//   apikey: "df09fe0573667300b510dfdfc988c884ef747757",
+
 // };
 export const environment = {
   production: false,
-  endPoint: "https://api-saads-dev.upds.edu.bo/archivo/api/",
-  urlLogin: 'https://login-dev.upds.edu.bo/SAADS-WEB?returnUrlLogin=http://localhost:4200/%23/login',
-  urlAccess: 'https://api-saads-dev.upds.edu.bo/access/api/',
+  endPoint: "https://api-saads-qa.upds.edu.bo/archivo/api/",
+  urlLogin: 'https://login-qa.upds.edu.bo/SAADS-WEB?returnUrlLogin=http://localhost:4200/%23/login',
+  urlAccess: 'https://api-saads-qa.upds.edu.bo/access/api/',
   urlFile: 'https://portal.upds.edu.bo/gapi.dev/upload/uploadasynccontainer/portal-notificacion',
-  endPointAccess: "https://portal.upds.edu.bo/gapi.dev/request/service/?path=access/api/",
-  endPointPortal: "http://172.16.248.33:8300/Portal/api/",
-  apikey: "df09fe0573667300b510dfdfc988c884ef747757",
-  objModNotificaciones: {
-    id: 34002,
-    nombre: "Noticias",
-    estado: true,
-    interfaces: [
-      {
-        "id": 41004,
-        "titulo": "Notificaciones",
-        "descripcion": " ",
-        "ubicacion": "notice",
-        "enlaceTutorial": null,
-        "tipo": 1,
-        "idModulo": 34002,
-        "icono": "pi-align-left",
-        "estado": true,
-        "tareas": [
-          {
-            "id": 40675,
-            "nombre": "CREAR",
-            "descripcion": "CREAR",
-            "idInterface": 41004,
-            "tipo": 1
-          },
-          {
-            "id": 40676,
-            "nombre": "EDITAR",
-            "descripcion": "EDITAR",
-            "idInterface": 41004,
-            "tipo": 3
-          },
-          {
-            "id": 40677,
-            "nombre": "ELIMINAR",
-            "descripcion": "ELIMINAR",
-            "idInterface": 41004,
-            "tipo": 4
-          },
-          {
-            "id": 40678,
-            "nombre": "LEER",
-            "descripcion": "LEER",
-            "idInterface": 41004,
-            "tipo": 2
-          },
-          {
-            "id": 40679,
-            "nombre": "TODO",
-            "descripcion": "LEER",
-            "idInterface": 41004,
-            "tipo": 5
-          }
-        ]
-      }]
-  }
+  apikey: "ed03510f786eec944f5849ee485acacf8da67e3c",
 };
--- src/environments/upds.qa.ts
+++ src/environments/upds.qa.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: true,
-    endPoint: "https://api-saads-qa.upds.edu.bo/archivo/api/",
-    urlLogin: 'https://login-qa.upds.edu.bo/SAADS-WEB?returnUrlLogin=http://localhost:4200/%23/login',
-    urlAccess: 'https://api-saads-qa.upds.edu.bo/access/api/',
-    urlFile: 'https://portal.upds.edu.bo/gapi.dev/upload/uploadasynccontainer/portal-notificacion',
-    apikey: "df09fe0573667300b510dfdfc988c884ef747757",    
-  };
+  production: true,
+  endPoint: "https://api-saads-qa.upds.edu.bo/archivo/api/",
+  urlLogin: 'https://login-qa.upds.edu.bo/SAADS-WEB?returnUrlLogin=https://saads-qa.upds.edu.bo/archivo/%23/login',
+  urlAccess: 'https://api-saads-qa.upds.edu.bo/access/api/',
+  urlFile: 'https://portal.upds.edu.bo/gapi.dev/upload/uploadasynccontainer/portal-notificacion',
+  apikey: "ed03510f786eec944f5849ee485acacf8da67e3c",
+};
