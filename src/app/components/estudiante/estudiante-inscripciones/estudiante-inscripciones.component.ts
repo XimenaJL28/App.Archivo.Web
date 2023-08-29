3a6d3182cf570ba20fdc4e90ba36fac386cefd9b
Mqe10 <mauricioquispeestrada@gmail.com>
2023-08-29 15:18:02 -0400

    filtro de seguridad en el componente de estudiante-inscripciones

--- src/app/components/estudiante/estudiante-inscripciones/estudiante-inscripciones.component.ts
+++ src/app/components/estudiante/estudiante-inscripciones/estudiante-inscripciones.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Inscripcion } from '../../../interfaces/estudiante.interface';
+import { MainService } from '../../../services/main.service';
 
 @Component({
   selector: 'app-estudiante-inscripciones',
@@ -11,10 +12,17 @@ export class EstudianteInscripcionesComponent {
   @Input() inscripciones: Inscripcion[] = [];
   @Input() inscripcion?: Inscripcion = undefined;
   @Output() getInscripcionSeleccionada: EventEmitter<Inscripcion> = new EventEmitter();
+  interfaz: any;
 
-  constructor() { }
+  constructor(private readonly MainService: MainService) {
+    this.interfaz = this.MainService.interfaces.filter((x: any) => x.id == 41006)[0];
+
+  }
 
   setInscripcionSeleccionada(inscripcion: Inscripcion): void {
     this.getInscripcionSeleccionada.emit(inscripcion);
+  }
+  permisoTipo(IdTarea: number) {
+    return this.interfaz.tareas.filter((x: any) => x.id == IdTarea).length > 0 ? true : false;
   }
 }
