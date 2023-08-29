63bd67a7f179512f2a7925438fec41da4ff1b3ed
XimenaJL28 <ximena.justiniano@UPDS.edu.bo>
2023-08-29 16:16:31 -0400

    Interación tarea en documento presentado

--- src/app/components/documento/documento-presentado/documento-presentado.component.ts
+++ src/app/components/documento/documento-presentado/documento-presentado.component.ts
@@ -3,6 +3,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DocumentoInscripcionCarrera } from '../../../interfaces/estudiante.interface';
 
 import { setColorDocumentoEstado } from '../../../utils/color';
+import { MainService } from '../../../services/main.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-documento-presentado',
@@ -13,12 +15,17 @@ export class DocumentoPresentadoComponent {
   @Input() documentos: DocumentoInscripcionCarrera[] = [];
   @Input() documento?: DocumentoInscripcionCarrera = undefined;
 
-  @Output() getDocumentoSeleccionado: EventEmitter<{documento: DocumentoInscripcionCarrera, update: boolean}> = new EventEmitter();
+  @Output() getDocumentoSeleccionado: EventEmitter<{ documento: DocumentoInscripcionCarrera, update: boolean }> = new EventEmitter();
   public visibleOperacionForm: boolean = false;
-  constructor() { }
+  interface: any;
+
+  constructor(private readonly MainService: MainService,
+    private messageService: MessageService) {
+    this.interface = this.MainService.interfaces.filter((x: any) => x.id == 41006)[0];
+  }
 
   setDocumentoSeleccionado(documentoInscripcioncarreraId: DocumentoInscripcionCarrera, update = false): void {
-    this.getDocumentoSeleccionado.emit({documento: documentoInscripcioncarreraId, update: update});
+    this.getDocumentoSeleccionado.emit({ documento: documentoInscripcioncarreraId, update: update });
   }
 
   getColorDocumentoEstado(documento: DocumentoInscripcionCarrera): string {
@@ -28,4 +35,15 @@ export class DocumentoPresentadoComponent {
   setVisibleOperacion() {
     this.visibleOperacionForm = true;
   }
+
+  permiso(idTarea: number) {
+    return this.interface.tareas.filter((x: any) => x.id == idTarea).length > 0 ? true : false;
+  }
+
+  getTarea() {
+    this.messageService.add({ severity: 'error', summary: 'No tiene muñeca', detail: 'No cuenta con permiso para acceder' });
+    console.log('No tiene muñeca');
+
+  }
+
 }
