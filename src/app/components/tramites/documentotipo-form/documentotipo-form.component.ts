66276b94631a340c105c429f298e25c6cbfc650b
XimenaJL28 <ximena.justiniano@UPDS.edu.bo>
2023-09-06 15:15:45 -0400

    Cambios DocumentoTipo

--- src/app/components/tramites/documentotipo-form/documentotipo-form.component.ts
+++ src/app/components/tramites/documentotipo-form/documentotipo-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
-import { DocumentoTipo, DocumentoRespaldo } from '../../../interfaces/tramite.interface';
+import { DocumentoTipo } from '../../../interfaces/tramite.interface';
 import { ListCarreraDTO } from '../../../interfaces/Plantilla.interface';
 
 import { DocumentoTipoService } from '../../../services/documento-tipo.service';
 import { PlantillaService } from '../../../services/plantilla.service';
+import { InputSwitchOnChangeEvent } from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-documentotipo-form',
@@ -16,7 +17,6 @@ import { PlantillaService } from '../../../services/plantilla.service';
 export class DocumentotipoFormComponent implements OnInit {
   @Output() cerrarDialogModal: EventEmitter<void> = new EventEmitter();
 
-  //public documentoRespaldo?: ListCarreraDTO = undefined;
   public documentoRespaldos: DocumentoTipo[] = [];
   public documentoTipos: DocumentoTipo[] = [];
   public documentoTipo!: DocumentoTipo;
@@ -29,109 +29,126 @@ export class DocumentotipoFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.documentoTipo = {
+      documentoTipoId: 0,
       nombre: '',
       estadoExpiracion: false,
-      documentoRespaldoId: 0
+      documentoRespaldoId: -1
     };
 
     this.documentoTipoService.getListDocumentoTipos()
       .then((response) => {
         this.documentoTipos = response || [];
-        console.log('documentotipo', this.documentoTipos);
-
-
-        /*let documentosRespaldos: DocumentoRespaldo[] = [
-          {
-            documentoRespaldoId: 0,
-            nombre: ''
-          }
-        ];
-
-        if (this.documentoTipos.length > 0) {
-          this.documentoTipos.forEach(item => {
-            if (item.documentoRespaldoId && item.documentoRespaldo) {
-              documentosRespaldos.push({
-                documentoRespaldoId: item.documentoRespaldoId,
-                nombre: item.documentoRespaldo
-              });
-            }
-          });
-
-          this.documentoRespaldos = [...documentosRespaldos];
-          if (this.documentoRespaldos.length > 0) {
-            this.documentoTipo.documentoRespaldoId = this.documentoRespaldos[0].documentoRespaldoId;
-          }
-        }*/
+        // this.documentoTipos = [
+        //   {
+        //     documentoTipoId: 1,
+        //     nombre: 'asdwrr',
+        //     estadoExpiracion: false,
+        //     documentoRespaldoId: -1
+        //   },
+        //   {
+        //     documentoTipoId: 3,
+        //     nombre: 'asdfsad',
+        //     estadoExpiracion: true,
+        //     documentoRespaldoId: -1
+        //   }
+        // ];
       });
 
     this.documentoTipoService.getListDocumentoTipos()
       .then((carrerer: DocumentoTipo[]) => {
-        this.documentoRespaldos = carrerer
+        this.documentoRespaldos = carrerer || [];
+
+        if (this.documentoRespaldos.length > 0) {
+          this.documentoTipo.documentoRespaldoId = this.documentoRespaldos[0].documentoTipoId || -1;
+        }
       }).catch((error: any) => {
         console.log(error);
       });
   }
 
-  seleccionarDocumentoTipo(documentoTipo: DocumentoTipo) {
-    this.documentoTipo = { ...documentoTipo };
-  }
+  async actualizarDocumentoExpiracion(evt: InputSwitchOnChangeEvent, documentoTipo: DocumentoTipo) {
+    if (!evt.checked) return;
 
-  async actualizarDocumentoExpiracion() {
-    if (!this.documentoTipo.documentoTipoId || this.documentoTipo.documentoTipoId < 1) {
+    const documentoTipoId = documentoTipo.documentoTipoId || 0;
+
+    if (documentoTipoId < 1) {
       this.messageService.add({ severity: 'error', summary: 'Datos no validos', detail: 'Revisar valores del documento' });
       return;
     }
 
-    const responseUpdate = await this.documentoTipoService.putEstadoExpiracionDocumentoTipo(
-      { tipoid: this.documentoTipo.documentoTipoId || 0 }
-    );
+    try {
+      const responseUpdate = await this.documentoTipoService.putEstadoExpiracionDocumentoTipo(
+        { tipoid: documentoTipoId }
+      );
+      console.log('documentotipo update', responseUpdate);
+      if (responseUpdate) {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Se ha registrado los documentos a la plantilla' });
+      }
+    } catch (error) {
+      this.messageService.add({ severity: 'error', summary: 'Verificar Datos', detail: 'Ha sucedido una excepción al modificar documento' });
+      return;
+    }
+    //this.cerrarDialogModal.emit();
+  }
 
-    console.log('documentotipo update', responseUpdate);
+  async guardarDocumentoTipo() {
+    // if (this.documentoTipo.nombre.trim().length < 3 ||
+    //   !this.documentoTipo.documentoRespaldoId ||
+    //   this.documentoTipo.documentoRespaldoId < 0
+    // ) {
+    //   this.messageService.add({ severity: 'error', summary: 'Datos no validos', detail: 'Revisar valores del documento' });
+    //   return;
+    // }
 
-    if (responseUpdate) {
+    const data: DocumentoTipo = {
+      nombre: this.documentoTipo.nombre,
+      estadoExpiracion: this.documentoTipo.estadoExpiracion,
+      documentoRespaldoId: this.documentoTipo.documentoRespaldoId,
+    }
+
+    const responseInsert = await this.documentoTipoService.postDocumentoTipo(data);
+    console.log('documentotipo save', responseInsert)
+    if (!responseInsert) {
       this.messageService.add({ severity: 'error', summary: 'Verificar Datos', detail: 'Ha sucedido una excepción al modificar documento' });
       return;
     }
 
-    this.cerrarDialogModal.emit();
+    this.documentoTipoService.getListDocumentoTipos()
+      .then((response) => {
+        this.documentoTipos = response || [];
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Se ha registrado los documentos a la plantilla' });
+      });
   }
 
-  async guardarDocumentoTipo() {
-    if (this.documentoTipo.nombre.trim().length < 3 ||
-      !this.documentoTipo.documentoRespaldoId) {
+  async actualizarDocumentoTipo(documentoTipo: DocumentoTipo) {
+    if (!documentoTipo.documentoRespaldoId || documentoTipo.documentoRespaldoId < 0) {
       this.messageService.add({ severity: 'error', summary: 'Datos no validos', detail: 'Revisar valores del documento' });
       return;
     }
 
-    const documentoTipoId = this.documentoTipo.documentoTipoId || 0;
-    // actualizar
-    if (documentoTipoId > 0) {
+    try {
       const responseUpdate = await this.documentoTipoService.putDocumentoTipo({
-        tipoid: this.documentoTipo.documentoTipoId,
-        respaldoid: this.documentoTipo.documentoRespaldoId,
+        tipoid: documentoTipo.documentoTipoId,
+        respaldoid: documentoTipo.documentoRespaldoId,
       });
-      console.log('documentotipo update', responseUpdate)
+      console.log('documentotipo update', responseUpdate);
+
       if (responseUpdate) {
-        this.messageService.add({ severity: 'error', summary: 'Verificar Datos', detail: 'Ha sucedido una excepción al modificar documento' });
-        return;
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Se ha registrado los documentos a la plantilla' });
       }
-
-      this.cerrarDialogModal.emit();
-    } else {
-      // insertar
-      const data: DocumentoTipo = {
-        nombre: this.documentoTipo.nombre,
-        estadoExpiracion: this.documentoTipo.estadoExpiracion,
-        documentoRespaldoId: this.documentoTipo.documentoRespaldoId,
-      }
-
-      const responseInsert = await this.documentoTipoService.postDocumentoTipo(data);
-      console.log('documentotipo save', responseInsert)
-      if (!responseInsert) {
-        this.messageService.add({ severity: 'error', summary: 'Verificar Datos', detail: 'Ha sucedido una excepción al modificar documento' });
-        return;
-      }
-      this.cerrarDialogModal.emit();
+    } catch (error) {
+      this.messageService.add({ severity: 'error', summary: 'Verificar Datos', detail: 'Ha sucedido una excepción al modificar documento' });
     }
+  }
+
+  nuevoElemento() {
+    const documentoRespaldoId = this.documentoRespaldos.length > 0 ? this.documentoRespaldos[0].documentoTipoId : -1;
+
+    this.documentoTipo = {
+      documentoTipoId: 0,
+      nombre: '',
+      estadoExpiracion: false,
+      documentoRespaldoId: documentoRespaldoId
+    };
   }
 }
