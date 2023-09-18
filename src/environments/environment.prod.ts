fc1b2e6f5a7b861598dfee83b50d01a5903b1f3e
Mqe10 <mauricioquispeestrada@gmail.com>
2023-09-18 15:15:55 -0400

    cambios en la cadena de conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  endPoint: "https://api-saads.upds.edu.bo/archivo/api/",
+  endPoint: "#{apiEndpoint}#",
   urlLogin: 'https://login.upds.edu.bo/SAADS-WEB?returnUrlLogin=https://saads.upds.edu.bo/archivo/%23/login',
   urlAccess: 'https://api-saads.upds.edu.bo/access/api/',
   urlFile: 'https://portal.upds.edu.bo/gapi/upload/uploadasynccontainer/portal-archivo',
-  apikey: "5SJE8rmwaw4rKz",
+  apikey: "#{apiKey}#",
 };
