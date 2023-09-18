e2365c477cd53b467f8a3d64519651cbb84cbb6d
Mqe10 <mauricioquispeestrada@gmail.com>
2023-09-18 15:52:38 -0400

    cambios endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endPoint: "#{apiEndpoint}#",
+  endPoint: "#{endPoint}#",
   urlLogin: '#{urlLogin}#',
   urlAccess: '#{urlAccess}#',
   urlFile: '#{urlFile}#',
